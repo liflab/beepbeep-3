c5097c1ea2c0bb779a313c455825d08e18924636
Sylvain Hallé <shalle@acm.org>
2015-07-31 09:09:46 -0400

    Partial parsing

--- Source/CoreTest/src/ca/uqac/lif/cep/eml/tuples/TuplesEmlTest.java
+++ Source/CoreTest/src/ca/uqac/lif/cep/eml/tuples/TuplesEmlTest.java
@@ -22,29 +22,41 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ca.uqac.lif.bullwinkle.BnfParser;
+import ca.uqac.lif.bullwinkle.BnfParser.InvalidGrammarException;
+import ca.uqac.lif.bullwinkle.ParseNode;
 import ca.uqac.lif.cep.eml.tuples.TupleGrammar;
 import ca.uqac.lif.cep.interpreter.GrammarExtension;
 import ca.uqac.lif.cep.interpreter.Interpreter;
 import ca.uqac.lif.cep.interpreter.Interpreter.ParseException;
+import ca.uqac.lif.util.PackageFileReader;
 
 public class TuplesEmlTest
-{
-	protected Interpreter m_interpreter;
+{	
+	protected BnfParser m_parser;
 	
 	@Before
 	public void setUp()
 	{
-		m_interpreter = new Interpreter();
-		GrammarExtension ext = new TupleGrammar();
-		m_interpreter.extendGrammar(ext);
+		try
+		{
+			m_parser.setGrammar(PackageFileReader.readPackageFile(Interpreter.class, "eml.bnf"));		
+			GrammarExtension ext = new TupleGrammar();
+			String productions = ext.getGrammar();
+			m_parser.setGrammar(productions);
+		}
+		catch (InvalidGrammarException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void testExtension1() throws ParseException
 	{
 		String expression = "0";
-		m_interpreter.setDebugMode(true);
-		Object result = m_interpreter.parseLanguage(expression);
+		Object result = m_parser.parse(expression);
 		assertNotNull(result);
 		assertTrue(result instanceof EmlNumber);
 	}
@@ -52,11 +64,15 @@ public class TuplesEmlTest
 	@Test
 	public void testExtension2() throws ParseException
 	{
-		String expression = "SELECT 0 FROM (0)";
-		m_interpreter.setDebugMode(true);
-		Object result = m_interpreter.parseLanguage(expression);
+		String expression = "SELECT 0 FROM 0";
+		Object result = shouldParse(expression, "<eml_select>");
 		assertNotNull(result);
 		assertTrue(result instanceof Select);
+	}
+	
+	protected void shouldParse(String expression, String start_symbol) throws ParseException
+	{
+		ParseNode pn = m_parser.parse(expression);
 	}
 	
 
