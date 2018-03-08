c6ce8d104209ade872a4874b1b467842f118c653
Sylvain Hallé <shalle@acm.org>
2018-03-08 13:30:34 -0500

    Added TurnInto

---
+++ Core/src/ca/uqac/lif/cep/functions/TurnInto.java
@@ -0,0 +1,79 @@
+/*
+    BeepBeep, an event stream processor
+    Copyright (C) 2008-2018 Sylvain Hallé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ca.uqac.lif.cep.functions;
+
+import java.util.Set;
+
+import ca.uqac.lif.cep.UniformProcessor;
+
+/**
+ * Turns any event into a predefined object.
+ * 
+ * @author Sylvain Hallé
+ */
+@SuppressWarnings("squid:S2160")
+public class TurnInto extends UniformProcessor
+{
+	/**
+	 * The event to turn everything into
+	 */
+	protected final Object m_event;
+
+	/**
+	 * Instantiates a new function processor
+	 * @param comp The computable object responsible for the computation
+	 */
+	public TurnInto(Object o)
+	{
+		super(1, 1);
+		m_event = o;
+	}
+
+	@Override
+	protected boolean compute(Object[] inputs, Object[] outputs)
+	{
+		outputs[0] = m_event;
+		return true;
+	}
+
+	@Override
+	public synchronized TurnInto duplicate()
+	{
+		TurnInto out = new TurnInto(m_event);
+		cloneInto(out);
+		return out;
+	}
+
+	@Override
+	public final void getInputTypesFor(/*@NotNull*/ Set<Class<?>> classes, int index)
+	{
+		classes.add(Object.class);
+	}
+
+	@Override
+	public final synchronized Class<?> getOutputType(int index)
+	{
+		return m_event.getClass();
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Turn into " + m_event;
+	}
+}
